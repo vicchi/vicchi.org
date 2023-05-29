ad0530252878728f99cdc7d6bb872757767aba99
Gary Gale <gary@vicchi.org>
2023-05-29 17:31:40 +0100

    feat: add listening to now page

--- site/themes/staticfy/assets/js/right-now.js
+++ site/themes/staticfy/assets/js/right-now.js
@@ -4,14 +4,14 @@ var vicchi = vicchi || {};
 
 vicchi.now = (function () {
     var rightnow = async function () {
-        var resp = await fetch(`${STATUS_API_URL}/static/statuslog.json`);
-        const status = await resp.json();
-
-        resp = await fetch(`${STATUS_API_URL}/v1/weather`);
-        const weather = await resp.json();
-
         var div = document.getElementById('dynamic-status');
         if (div !== null) {
+            var resp = await fetch(`${STATUS_API_URL}/static/statuslog.json`);
+            const status = await resp.json();
+
+            resp = await fetch(`${STATUS_API_URL}/v1/weather`);
+            const weather = await resp.json();
+
             var statuses = [];
             var elem = document.createElement('p');
             elem.setAttribute('class', 'mb-0');
@@ -33,10 +33,48 @@ vicchi.now = (function () {
         }
     };
 
+    var musicnow = async function () {
+        var div = document.getElementById('now-music');
+        if (div !== null) {
+            var resp = await fetch(`${STATUS_API_URL}/v1/listening`);
+            const music = await resp.json();
+
+            music['tracks'].forEach(function (track, i) {
+                document.getElementById(`track-${i}-url`).href = track['url'];
+                document.getElementById(`track-${i}-url`).setAttribute('title', track['track']);
+                document.getElementById(`track-${i}-name`).innerHTML = track['track'];
+                document.getElementById(`track-${i}-artist`).innerHTML = track['artist'];
+                document.getElementById(`track-${i}-image`).src = track['image'];
+                document.getElementById(`track-${i}-image`).alt = track['track'];
+            });
+
+            music['artists'].forEach(function (artist, i) {
+                document.getElementById(`artist-${i}-url`).href = artist['url'];
+                document.getElementById(`artist-${i}-url`).setAttribute('title', artist['name']);
+                document.getElementById(`artist-${i}-name`).innerHTML = artist['name'];
+                document.getElementById(`artist-${i}-count`).innerHTML = `${artist['count']} times`;
+                document.getElementById(`artist-${i}-image`).src = artist['image'];
+                document.getElementById(`artist-${i}-image`).alt = artist['name'];
+            });
+
+            music['releases'].forEach(function (release, i) {
+                document.getElementById(`release-${i}-url`).href = release['url'];
+                document.getElementById(`release-${i}-url`).setAttribute('title', release['name']);
+                document.getElementById(`release-${i}-name`).innerHTML = release['release'];
+                document.getElementById(`release-${i}-artist`).innerHTML = release['artist'];
+                document.getElementById(`release-${i}-image`).src = release['image'];
+                document.getElementById(`release-${i}-image`).alt = release['release'];
+            });
+        }
+    };
+
     return {
-        rightnow: rightnow
+        rightnow: rightnow,
+        musicnow: musicnow
     };
 })();
-window.onload = (event) => {
+
+window.addEventListener('load', function (event) {
     vicchi.now.rightnow();
-};
+    vicchi.now.musicnow();
+});
